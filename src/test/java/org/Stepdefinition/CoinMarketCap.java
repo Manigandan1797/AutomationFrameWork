149ec187ba8efa753bd8d228a77d0c050187bdad
Manigandan1797 <121866205+Manigandan1797@users.noreply.github.com>
2023-01-04 21:23:51 +0530

    Update CoinMarketCap.java

--- src/test/java/org/Stepdefinition/CoinMarketCap.java
+++ src/test/java/org/Stepdefinition/CoinMarketCap.java
@@ -24,12 +24,12 @@ public class CoinMarketCap extends BaseClass  {
 	 	
 	@Given("The user should launch the browser")
 	public void theUserShouldLaunchTheBrowser() {
-		initialization();
+	    initialization();
 	}
 
 	@When("The user should load the url")
 	public void theUserShouldLoadTheUrl() {
-		loadurl("https://coinmarketcap.com");
+	    loadurl("https://coinmarketcap.com");
 	    winmax();
 	    pom.jsclick(pom.popHandle1,driver);
 	    pom.jsclick(pom.popHandle2,driver);
@@ -37,12 +37,12 @@ public class CoinMarketCap extends BaseClass  {
 
 	@When("The user should select the chart in to list of {string}")
 	public void theUserShouldSelectTheChartInToListOf(String int1) throws InterruptedException {
-		pom.jsclick(pom.dropDown, driver);
-		Thread.sleep(3000);
-		WebElement dropDownValue = driver.findElement(By.xpath("//div[@class='sc-aef7b723-0 sc-f84d4cca-0 ezlhyG dropdown-container']//button[text()='"+int1+"']"));
-		JavascriptExecutor js=(JavascriptExecutor) driver;
-		js.executeScript("arguments[0].click();", dropDownValue);
-	       pom.jsclick(pom.showRow,driver);
+	    pom.jsclick(pom.dropDown, driver);
+	    Thread.sleep(3000);
+	    WebElement dropDownValue = driver.findElement(By.xpath("//div[@class='sc-aef7b723-0 sc-f84d4cca-0 ezlhyG dropdown-container']//button[text()='"+int1+"']"));
+	    JavascriptExecutor js=(JavascriptExecutor) driver;
+	    js.executeScript("arguments[0].click();", dropDownValue);
+	    pom.jsclick(pom.showRow,driver);
 	}
 	@When("The user should filter by {string}")
 	public void theUserShouldFilterByAlgoPow(String Value) throws InterruptedException {
@@ -52,46 +52,46 @@ public class CoinMarketCap extends BaseClass  {
 	}
 	@When("The user should capture the information from table")
 	public void theUserShouldCaptureTheInformationFromTable() {
-		pom.elementVisible(pom.name, driver);
+	   pom.elementVisible(pom.name, driver);
 	}
 
 	@When("The user should click on addfilter")
 	public void theUserShouldClickOnAddfilter() throws InterruptedException {
-		Thread.sleep(5000);
-		WebElement filterBtn = driver.findElement(By.xpath("//div[@class='sc-319be8a1-5 feBTJO']//button[text()='Filters']"));
-		JavascriptExecutor js=(JavascriptExecutor) driver;
-		js.executeScript("arguments[0].click();", filterBtn);   
+	    Thread.sleep(5000);
+	    WebElement filterBtn = driver.findElement(By.xpath("//div[@class='sc-319be8a1-5 feBTJO']//button[text()='Filters']"));
+	    JavascriptExecutor js=(JavascriptExecutor) driver;
+	    js.executeScript("arguments[0].click();", filterBtn);   
 	}
 	
 	@When("The user should click on toggle mineable")
 	public void theUserShouldClickOnToggleMineable() {
-		pom.jsclick(pom.AddFilter,driver);
-		pom.jsclick(pom.minableToggle,driver);
+	    pom.jsclick(pom.AddFilter,driver);
+	    pom.jsclick(pom.minableToggle,driver);
 	}
 
 	@When("The user should be select all Cryptocurrencies")
 	public void theUserShouldBeSelectAllCryptocurrencies() {
-	   	pom.forceClick(pom.AllCryptoDropDown,driver);
+	   pom.forceClick(pom.AllCryptoDropDown,driver);
 		
 	}
 
 	@When("The user should be select coins")
 	public void theUserShouldBeSelectCoins() throws InterruptedException {
-		pom.forceClick(pom.Coins,driver);
+	   pom.forceClick(pom.Coins,driver);
 	}
 
 	@When("The user should be select price and set min value")
 	public void theUserShouldBeSelectPriceAndSetMinValueTo() throws InterruptedException {
-		pom.jsclick(pom.Price,driver);
-		pom.enterValues(pom.minPrice, "100", driver);
-		pom.enterValues(pom.maxPrice, "10000", driver);
-		pom.jsclick(pom.ApplyFilter,driver);
-		pom.jsclick(pom.ShowResult,driver);
+	    pom.jsclick(pom.Price,driver);
+	    pom.enterValues(pom.minPrice, "100", driver);
+	    pom.enterValues(pom.maxPrice, "10000", driver);
+	    pom.jsclick(pom.ApplyFilter,driver);
+	    pom.jsclick(pom.ShowResult,driver);
 	}
 
 	@When("The user should compare price name and position")
 	public void theUserShouldComparePriceNameAndPosition() {
-		pom.elementVisible(pom.name, driver);
+	    pom.elementVisible(pom.name, driver);
 	}
 
 	@Then("The user should close the browser")
